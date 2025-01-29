fc95a1a5ea6262ac6290bf2148900e2e48c6835b
Youssef3_ <46034687+youssefjojo@users.noreply.github.com>
2025-01-30 01:01:26 +0200

    finished

--- js/bondi.js
+++ js/bondi.js
@@ -10,3 +10,22 @@ listItems.forEach(item => {
         item.classList.add('active');
     });
     });
+
+const searchIcon = document.getElementsByClassName("search-icon")[0];
+const searchBox = document.getElementsByClassName("nav-bar-search-box")[0];
+const search = document.getElementsByClassName("navbar-search")[0];
+
+searchIcon.addEventListener("click", () => {
+    searchIcon.classList.remove("d-lg-block");
+    searchBox.style.width = "280px";
+    search.style.transform = "translateX(0)";
+    setTimeout(() => {
+        search.focus(); // Focus after 0.3s delay
+    }, 300);
+});
+
+search.addEventListener("blur", () => {
+    searchIcon.classList.add("d-lg-block");
+    searchBox.style.width = "0px";
+    search.style.transform = "translateX(calc(100% + 15px))"; // Hide it smoothly
+});
